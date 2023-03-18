2d76e0116744ef9dd42657406ca67aa2651163af
unknown-bytes <unknown.develops@gmail.com>
2023-03-18 13:01:47 -0400

    ...

---
+++ models/Fruits.js
@@ -0,0 +1,32 @@
+fruits = [
+    {
+        name:'apple',
+        color: 'red',
+        readyToEat: true
+    },
+    {
+        name:'pear',
+        color: 'green',
+        readyToEat: false
+    },
+    {
+        name:'banana',
+        color: 'yellow',
+        readyToEat: true
+    }
+];
+
+starWarsShips = [
+    {
+        name: "Death Star",
+        destroyed: true
+    }, {
+        name: "Millenium Falcon",
+        destroyed: false
+    }
+]
+
+module.exports = {
+    fruits: fruits,
+    starWarsShips: starWarsShips
+}
